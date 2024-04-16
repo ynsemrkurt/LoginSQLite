4872bd6e6ae6d23f83ec9ec05c7e174e5edd7346
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-04-16 18:41:02 +0300

    Complete Home Page Code

--- app/src/main/java/com/example/loginsqlite/HomeActivity.kt
+++ app/src/main/java/com/example/loginsqlite/HomeActivity.kt
@@ -1,10 +1,12 @@
 package com.example.loginsqlite
 
+import FruitsAdapter
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.loginsqlite.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -14,5 +16,9 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val fruits= listOf("Banana", "Apple", "Orange", "Mango", "Grapes","Strawberry","Mandarin")
+        binding.fruitsRecycleView.layoutManager= LinearLayoutManager(this)
+        binding.fruitsRecycleView.adapter=FruitsAdapter(fruits)
     }
 }
