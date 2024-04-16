e897b522111c7b0bec1d1e3a7331a0725384ca8d
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-04-16 18:55:58 +0300

    Added Add Fruit Function

--- app/src/main/java/com/example/loginsqlite/HomeActivity.kt
+++ app/src/main/java/com/example/loginsqlite/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.example.loginsqlite
 
 import FruitsAdapter
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.loginsqlite.databinding.ActivityHomeBinding
@@ -9,12 +10,26 @@ import com.example.loginsqlite.databinding.ActivityHomeBinding
 class HomeActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityHomeBinding
+    val fruits: MutableList<String> = mutableListOf("Banana", "Apple", "Orange", "Mango", "Grapes","Strawberry","Mandarin")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val fruits= listOf("Banana", "Apple", "Orange", "Mango", "Grapes","Strawberry","Mandarin")
+        adapterList()
+    }
+
+    fun addFruit(view: View) {
+        val fruitName=binding.addFruitEditText.text.toString().trim()
+        if(fruitName.isNotEmpty()){
+            fruits.add(fruitName)
+            adapterList()
+            binding.addFruitEditText.text.clear()
+        }
+    }
+
+    fun adapterList(){
         binding.fruitsRecycleView.layoutManager= LinearLayoutManager(this)
         binding.fruitsRecycleView.adapter=FruitsAdapter(fruits)
     }
