80f6f3492e5954bedfe73a80a9fc270abd923cec
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-04-16 17:46:52 +0300

    Create Home Activity

---
+++ app/src/main/java/com/example/loginsqlite/HomeActivity.kt
@@ -0,0 +1,18 @@
+package com.example.loginsqlite
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.loginsqlite.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
